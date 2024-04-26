4fa6a08220e0c2223249484601a023879a1ce1da
= <mm.jansen@fontys.nl>
2024-04-26 12:51:30 +0200

    update: changed function name

--- R/read_vlpbek_data.R
+++ R/read_vlpbek_data.R
@@ -5,21 +5,21 @@
 ################################################################################
 
 # helper function
-is_vlpbek <- function(chosen_locationname) {
+is_vlpbek_data <- function(chosen_locationname) {
   implied_filename <- stringr::str_sub(chosen_locationname,
     start = -29,
     end = -1
   )
   # should start with "VLPBEK_" and should end with ".csv"
-  looks_like_vlpbek <- stringr::str_sub(implied_filename, start = 1, end = 7) == "VLPBEK_" &
+  looks_like_vlpbek_data <- stringr::str_sub(implied_filename, start = 1, end = 7) == "VLPBEK_" &
     stringr::str_sub(implied_filename, start = -4, end = -1) == ".csv"
-  looks_like_vlpbek
+  looks_like_vlpbek_data
 }
 
 
 #' Read in a vlpbek file
 #'
-#' `read_vlpbek` reads in a vlpbek type of file (bekostigingsbestand), containing interim data about funding.
+#' `read_vlpbek_data` reads in a vlpbek type of file (bekostigingsbestand), containing interim data about funding.
 #'  The function opens a window with which a file can be chosen.
 #'
 #' @param locationname_vlpbek location of the vlpbek type of file (bekostigingsbestand)
@@ -27,9 +27,9 @@ is_vlpbek <- function(chosen_locationname) {
 #' @return a dataframe containing the raw vlpbek file
 #' @export
 #'
-read_vlpbek <- function(locationname_vlpbek) {
+read_vlpbek_data <- function(locationname_vlpbek) {
 
-  if (!is_vlpbek(locationname_vlpbek)) stop("The name of the chosen file is not compatible with a VLPBEK file. Be sure to select a .csv file that starts with `VLPBEK_`.")
+  if (!is_vlpbek_data(locationname_vlpbek)) stop("The name of the chosen file is not compatible with a VLPBEK file. Be sure to select a .csv file that starts with `VLPBEK_`.")
 
   vlpbek <- utils::read.csv(
     file = locationname_vlpbek,
--- tests/testthat/test-read_vlpbek.R
+++
@@ -1,3 +0,0 @@
-test_that("vlpbek file can be read in", {
-  expect_error(read_vlpbek("not_the_correct_file"))
-})
